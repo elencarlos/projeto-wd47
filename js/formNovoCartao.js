2dd2df36a9eb3f95b32e1ee754d3d14a12833b9d
Elencarlos Soares <elencarlos@uft.edu.br>
2018-12-12 10:13:40 -0300

    validacao formulario

--- js/formNovoCartao.js
+++ js/formNovoCartao.js
@@ -1,3 +1,25 @@
 ;(function () {
-    
+    const formulario = document.querySelector('.formNovoCartao');
+    const textCartao = document.querySelector('.formNovoCartao-conteudo');
+    formulario.addEventListener('submit',function (event) {
+        event.preventDefault();
+
+        if(textCartao.value.trim()){
+            console.log(textCartao.value);
+        }
+        else{
+            const temMsg = formulario.querySelector('.formNovoCartao-msg');
+            if(!temMsg){
+                const mensagem = document.createElement('p');
+                mensagem.classList.add('formNovoCartao-msg');
+                mensagem.textContent = 'Você precisa digitar algo';
+                //this.insertBefore(mensagem,textCartao);
+                textCartao.insertAdjacentElement('afterend',mensagem);
+                mensagem.addEventListener('animationend',function () {
+                    this.remove();
+                })
+            }
+        }
+
+    })
 })();
